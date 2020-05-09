543112246605b6820c03ec9e427f74bc06e46c3b
wangzishun <wangzishun@58.com>
2020-05-09 16:34:42 +0800

    feat(wangzi): add output management

---
+++ webpack-guides-demos/src/print.js
@@ -0,0 +1,10 @@
+/*
+ * @Author       : wangzishun
+ * @Date         : 2020-05-09 16:14:54
+ * @LastEditors  : wangzishun
+ * @LastEditTime : 2020-05-09 16:14:54
+ * @Description  :
+ */
+export default function printMe() {
+  console.log('I get called from print.js!');
+}
